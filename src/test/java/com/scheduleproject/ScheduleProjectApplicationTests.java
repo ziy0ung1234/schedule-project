40a9f971fcf10d90be6bb81f061a12827338686f
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2025-11-08 18:36:20 +0900

    Initial commit

---
+++ src/test/java/com/scheduleproject/ScheduleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
